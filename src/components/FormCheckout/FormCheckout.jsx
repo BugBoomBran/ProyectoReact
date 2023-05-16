484a795fda2dd16083cbd33c693fe647462daaf9
Brandon Acosta <brandon44acosta@gmail.com>
2023-05-16 13:02:24 -0300

    formik y yup

--- src/components/FormCheckout/FormCheckout.jsx
+++ src/components/FormCheckout/FormCheckout.jsx
@@ -2,25 +2,55 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { Button, TextField } from "@mui/material";
 
-const FormCheckout = ({ handleSubmit, handleChange }) => {
+const FormCheckout = ({ handleSubmit, handleChange, errors, values }) => {
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
-        <Grid container spacing={2} padding={2}>
-          <Grid item xs={12} sm={6} lg={4}>
-            <TextField id="outlined-basic" label="Names" name="Names" variant="outlined" fullWidth onChange={handleChange} />
+        <Grid container spacing={2} padding={2} flex justifyContent={"center"}>
+          <Grid item xs={11} sm={7}>
+            <TextField id="outlined-basic" label="Names" name="names" variant="outlined" fullWidth onChange={handleChange} error={errors.names ? true : false} helperText={errors.names} />
           </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <TextField id="outlined-basic" label="Email" name="Email" variant="outlined" fullWidth onChange={handleChange} />
+          <Grid item xs={11} sm={7}>
+            <TextField id="outlined-basic" label="Email" name="email" variant="outlined" fullWidth onChange={handleChange} error={errors.email ? true : false} helperText={errors.email} />
           </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <TextField id="outlined-basic" label="Password" name="Password" variant="outlined" fullWidth onChange={handleChange} />
+          <Grid item xs={11} sm={7}>
+            <TextField id="outlined-basic" label="Phone" name="phone" variant="outlined" fullWidth onChange={handleChange} error={errors.phone ? true : false} helperText={errors.phone} />
           </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <TextField id="outlined-basic" label="Phone" name="Phone" variant="outlined" fullWidth onChange={handleChange} />
+          <Grid item xs={11} sm={7}>
+            <TextField id="outlined-basic" label="Password" name="password" variant="outlined" fullWidth onChange={handleChange} error={errors.password ? true : false} helperText={errors.password} />
           </Grid>
-          <Grid item xs={12} sm={6} lg={4}>
-            <Button variant="contained">Comprar</Button>
+          {values.password.length > 0 && (
+            <Grid item xs={11} sm={7}>
+              <TextField id="outlined-basic" label="Confirm Password" name="confirmPassword" variant="outlined" fullWidth onChange={handleChange} error={errors.confirmPassword ? true : false} helperText={errors.confirmPassword} />
+            </Grid>
+          )}
+          <Grid item xs={11} sm={7}>
+            <TextField id="outlined-basic" label="Card Name" name="cardName" variant="outlined" fullWidth onChange={handleChange} error={errors.cardName ? true : false} helperText={errors.cardName} />
+          </Grid>
+          {values.cardName.length > 0 && (
+            <Grid item xs={11} sm={7}>
+              <TextField id="outlined-basic" label="Card Number" name="cardNumber" variant="outlined" fullWidth onChange={handleChange} error={errors.cardNumber ? true : false} helperText={errors.cardNumber} />
+            </Grid>
+          )}
+          {values.cardName.length > 0 && (
+            <Grid item xs={11} sm={7}>
+              <TextField id="outlined-basic" label="Expiration Date" name="expirationDate" variant="outlined" fullWidth onChange={handleChange} error={errors.expirationDate ? true : false} helperText={errors.expirationDate} />
+            </Grid>
+          )}
+          {values.cardName.length > 0 && (
+            <Grid item xs={11} sm={7}>
+              <TextField id="outlined-basic" label="Security Code" name="securityCode" variant="outlined" fullWidth onChange={handleChange} error={errors.securityCode ? true : false} helperText={errors.securityCode} />
+            </Grid>
+          )}
+          {values.cardName.length > 0 && (
+            <Grid item xs={11} sm={7}>
+              <TextField id="outlined-basic" label="Identity Card" name="identityCard" variant="outlined" fullWidth onChange={handleChange} error={errors.identityCard ? true : false} helperText={errors.identityCard} />
+            </Grid>
+          )}
+          <Grid item xs={7}>
+            <Button type="submit" variant="contained">
+              Comprar
+            </Button>
           </Grid>
         </Grid>
       </form>
--- src/components/FormCheckout/FormCheckoutContainer.jsx
+++ src/components/FormCheckout/FormCheckoutContainer.jsx
@@ -4,24 +4,45 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const FormCheckoutContainer = () => {
-  const { handleSubmit, handleChange } = useFormik({
+  const { handleSubmit, handleChange, errors, values } = useFormik({
     initialValues: {
-      name: "",
+      names: "",
       email: "",
-      password: "",
       phone: "",
+      password: "",
+      confirmPassword: "",
+      cardName: "",
+      cardNumber: "",
+      expirationDate: "",
+      securityCode: "",
+      identityCard: "",
     },
     onSubmit: (data) => {
       console.log(data);
     },
     validationSchema: Yup.object().shape({
-      nombre: Yup.string().required(),
+      names: Yup.string().required("este campo es obligatorio").min(3, "el nombre debe tener al menos 3 caracteres").max(12, "El nombre no puede superar los 12 caracteres"),
+      email: Yup.string().required("este campo es obligatorio").email("este campo debe ser un email"),
+      phone: Yup.number().required("este campo es obligatorio"),
+      password: Yup.string()
+        .required("este campo es obligatorio")
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/, { message: "la contraseña debe tener al menos 1 mayuscula, 1 minuscula, 1 numero, entre 6 y 15 caracteres" }),
+      confirmPassword: Yup.string()
+        .required("este campo es obligatorio")
+        .oneOf([Yup.ref("password")], "las contraseñas no coinciden"),
+      cardName: Yup.string().min(3, "el nombre debe tener al menos 3 caracteres").max(12, "El nombre no puede superar los 12 caracteres"),
+      // cardName no es requerido por si el cliente quiere pagar cuando lo retire
+      cardNumber: Yup.number().required("este campo es obligatorio"),
+      expirationDate: Yup.number().required("este campo es obligatorio").min(4, "este campo debe tener 4 caracteres").max(4, "este campo debe tener 4 caracteres"),
+      securityCode: Yup.number().required("este campo es obligatorio").min(3, "este campo debe tener 3 caracteres").max(3, "este campo debe tener 3 caracteres"),
+      identityCard: Yup.number().required("este campo es obligatorio"),
     }),
+    validateOnChange: false,
   });
 
   return (
     <div>
-      <FormCheckout handleSubmit={handleSubmit} handleChange={handleChange} />
+      <FormCheckout values={values} handleSubmit={handleSubmit} handleChange={handleChange} errors={errors} />
     </div>
   );
 };

661ece1bc478b949ae45a5b909eb5c79b4d26cdf
Brandon Acosta <brandon44acosta@gmail.com>
2023-05-18 00:49:38 -0300

    stockSwal

--- src/components/Counter/CounterContainer.jsx
+++ src/components/Counter/CounterContainer.jsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
 import Counter from "./Counter";
+import Swal from "sweetalert2";
 
 const CounterContainer = ({ stock, onAdd, initial = 1 }) => {
   const [counter, setCounter] = useState(initial);
+
+  const stockSwal = () => {
+    Swal.fire({
+      position: "top",
+      icon: "info",
+      title: "Has alcanzado el stock máximo",
+      showConfirmButton: true,
+      timer: 1500,
+      color: "#297ea6",
+    });
+  };
 
   useEffect(() => {
     setCounter(initial);
   }, [initial]);
 
   const sumar = () => {
-    counter < stock ? setCounter(counter + 1) : alert("stock maximo");
+    counter < stock ? setCounter(counter + 1) : <div>{stockSwal()}</div>;
   };
   const restar = () => {
     if (counter > 1) setCounter(counter - 1);

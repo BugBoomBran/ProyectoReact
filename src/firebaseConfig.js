255c9906dd4531717ee570f05cc35e90dd24af2b
Brandon Acosta <brandon44acosta@gmail.com>
2023-05-17 23:14:59 -0300

    readme

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_LyheRICL7mQzhv7NAPsoUUXv4nuan3M",
-  authDomain: "react-coder-house-91306.firebaseapp.com",
-  projectId: "react-coder-house-91306",
-  storageBucket: "react-coder-house-91306.appspot.com",
-  messagingSenderId: "867789588715",
-  appId: "1:867789588715:web:67c748c8ed9a29aa7e56ae",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
